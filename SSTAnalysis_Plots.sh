8c64c9a85cf4ccdc23c16a732109dab11b408916
Pedro Velez <pvelezbelchi@gmail.com>
2023-05-04 19:10:51 +0200

    initial

---
+++ SSTAnalysis_Plots.sh
@@ -0,0 +1,18 @@
+#! /bin/zsh
+source $HOME/.zshrc
+source $HOME/.telegram
+
+conda activate ocean
+/Users/pvb/miniconda3/envs/ocean/bin/python SSTAnalysis_daily_Plots.py
+
+
+URL="https://api.telegram.org/bot$ArgoEsBotTOKEN/sendMessage"
+URLimg="https://api.telegram.org/bot$ArgoEsBotTOKEN/sendphoto?chat_id=$ArgoEsChannel"
+MENSAJE="Global Analisis SST"
+
+curl -s -X POST $URL -d chat_id=$ArgoEsChannel -d text="$MENSAJE" -d parse_mode=html
+curl -F "photo=@$HOME/Desktop/sstd_GO_mean.png" $URLimg -F caption="Global SST mean anomlay"
+curl -F "photo=@$HOME/Desktop/sstd_anom_GO_mean.png" $URLimg -F caption="Global SST mean anomlay"
+curl -F "photo=@$HOME/Desktop/sstd_GO.png" $URLimg -F caption="Global SST mean anomlay"
+curl -F "photo=@$HOME/Desktop/sstd_anom_GO.png" $URLimg -F caption="Global SST mean anomlay"
+
