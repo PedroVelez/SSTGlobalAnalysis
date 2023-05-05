4997d4267ca7e36a7b7f62c03c7ac5e4f105286c
Pedro Velez <pvelezbelchi@gmail.com>
2023-05-05 18:31:21 +0000

    change sh

--- SSTAnalysis_Plots.sh
+++ SSTAnalysis_Plots.sh
@@ -11,8 +11,8 @@ URLimg="https://api.telegram.org/bot$ArgoEsBotTOKEN/sendphoto?chat_id=$ArgoEsCha
 MENSAJE="Global Analisis SST"
 
 curl -s -X POST $URL -d chat_id=$ArgoEsChannel -d text="$MENSAJE" -d parse_mode=html
-curl -F "photo=@$HOME/Desktop/sstd_GO_mean.png" $URLimg -F caption="Global SST mean anomlay"
-curl -F "photo=@$HOME/Desktop/sstd_anom_GO_mean.png" $URLimg -F caption="Global SST mean anomlay"
-curl -F "photo=@$HOME/Desktop/sstd_GO.png" $URLimg -F caption="Global SST mean anomlay"
-curl -F "photo=@$HOME/Desktop/sstd_anom_GO.png" $URLimg -F caption="Global SST mean anomlay"
+curl -F "photo=@$HOME/Analisis/SSTGlobalAnalysis/images/sstd_GO_mean.png" $URLimg -F caption="Global SST mean anomlay"
+curl -F "photo=@$HOME/Analisis/SSTGlobalAnalysis/images/sstd_anom_GO_mean.png" $URLimg -F caption="Global SST mean anomlay"
+curl -F "photo=@$HOME/Analisis/SSTGlobalAnalysis/images/sstd_GO.png" $URLimg -F caption="Global SST mean anomlay"
+curl -F "photo=@$HOME/Analisis/SSTGlobalAnalysis/images/sstd_anom_GO.png" $URLimg -F caption="Global SST mean anomlay"
 
