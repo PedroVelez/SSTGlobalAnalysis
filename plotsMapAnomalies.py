0eec4cfaa1db6282e6b9f09e67192ec9c49156fa
Pedro Velez <pvelezbelchi@gmail.com>
2024-05-21 22:27:01 +0100

    update

--- plotsMapAnomalies.py
+++ plotsMapAnomalies.py
@@ -17,8 +17,8 @@ if os.uname().nodename.lower().find('eemmmbp') != -1:
 elif os.uname().nodename.lower().find('rossby') != -1:
     analisisDir = '/home/pvb/Analisis/SSTGlobalAnalysis'
 
-Titulos = ['Oceano Global','AtlanticoNorte' , 'Demarcación marina levantino-balear','Demarcación marina noratlántica','Demarcación marina canaria']
-Titulos_short = ['GO','NAtl','LEBA','NOR','CAN']
+Titulos = ['Oceano Global','Hemisferio norte','Hemisferio sur','AtlanticoNorte', 'Demarcación marina levantino-balear', 'Demarcación marina noratlántica','Demarcación marina canaria','Demarcación sudatlántica','Demarcación Estrecho y Alborán']
+Titulos_short = ['GO','NH','SH','NAtl','LEBA', 'NOR','CAN','SUD','ESAL']
 
 
 for i in range(0,len(Titulos)):
