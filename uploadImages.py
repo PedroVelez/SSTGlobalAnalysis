fcc7fdfd94b6541e5798c6ed0172b944e3754edf
Pedro Velez <pvelezbelchi@gmail.com>
2024-09-28 00:23:51 +0100

    Add demarcaciones map

--- uploadImages.py
+++ uploadImages.py
@@ -19,24 +19,33 @@ elif os.uname().nodename.lower().find('rossby') != -1:
     imagesDir = dirAnalisis + '/SSTGlobalAnalysis/images'
 
 os.chdir(imagesDir)
-## 
+
+## Global analisis
 session = ftplib.FTP('ftp.oceanografia.es',userFTP,passwdFTP)
 session.cwd('/html/pedro/images/SST')
+Titulos_short = ['GO','NH','SH','NAtl']
+
 #Read and uplod files
-filenames = glob.glob(imagesDir+'/*.png')
-for filename in filenames:
-    print('https://www.oceanografia.es/pedro/images/SST/'+filename.split('/')[-1])
-    session.storbinary('STOR '+ filename.split('/')[-1], open(filename, 'rb'))
+for it in range(0,len(Titulos_short)):
+    titulo_short = Titulos_short[it]
+    filenames = glob.glob(imagesDir+'/*'+titulo_short+'*.png')
+    for filename in filenames:
+        print('https://www.oceanografia.es/pedro/images/SST/'+filename.split('/')[-1])
+        session.storbinary('STOR '+ filename.split('/')[-1], open(filename, 'rb'))
 
 session.quit()    
 
-## 
+## Demarcaciones
 session = ftplib.FTP('ftp.oceanografia.es',userFTP,passwdFTP)
 session.cwd('/html/IEOOS/SST/images')
+Titulos_short = ['LEB', 'NOR','CAN','SUD','ESA']
+
 #Read and uplod files
-filenames = glob.glob(imagesDir+'/*.png')
-for filename in filenames:
-    print('https://www.oceanografia.es/pedro/images/SST/'+filename.split('/')[-1])
-    session.storbinary('STOR '+ filename.split('/')[-1], open(filename, 'rb'))
+for it in range(0,len(Titulos_short)):
+    titulo_short = Titulos_short[it]
+    filenames = glob.glob(imagesDir+'/*'+titulo_short+'*.png')
+    for filename in filenames:
+        print('https://www.oceanografia.es/IEOOS/SST/images/'+filename.split('/')[-1])
+        session.storbinary('STOR '+ filename.split('/')[-1], open(filename, 'rb'))
 
 session.quit()    
